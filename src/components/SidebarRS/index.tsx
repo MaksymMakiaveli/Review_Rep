46727d3e54ade4e2668d6d138447208617330862
mac <mac@macs-MacBook-Pro.local>
2022-02-03 21:11:37 +0200

    update sidebar

--- src/components/SidebarRS/index.tsx
+++ src/components/SidebarRS/index.tsx
@@ -20,6 +20,7 @@ import { NavLink } from 'react-router-dom';
 interface SidebarRSProps {}
 
 type LinkListType = {
+  title: string;
   link: string;
   eventKey?: string;
   icon?: JSX.Element;
@@ -28,6 +29,7 @@ type LinkListType = {
 
 const linkListDashboard: LinkListType[] = [
   {
+    title: 'Dashboard',
     link: 'Dashboard',
     icon: <Dashboard />,
     eventKey: '1',
@@ -35,21 +37,25 @@ const linkListDashboard: LinkListType[] = [
 ];
 const linkListManagement: LinkListType[] = [
   {
+    title: 'Asset',
     link: 'Asset',
     icon: <Asset />,
     eventKey: '2',
   },
   {
+    title: 'Work Orders',
     link: 'WorkOrders',
     icon: <WorkOrders />,
     eventKey: '3',
   },
   {
+    title: 'Depreciation',
     link: 'Depreciation',
     icon: <Depreciation />,
     eventKey: '4',
   },
   {
+    title: 'Supplies',
     link: 'Supplies',
     icon: <Supplies />,
     eventKey: '5',
@@ -57,41 +63,48 @@ const linkListManagement: LinkListType[] = [
 ];
 const linkListProperties: LinkListType[] = [
   {
+    title: 'Attributes',
     link: 'Attributes',
     icon: <Attributes />,
     eventKey: '6',
   },
   {
+    title: 'Companies',
     link: 'Companies',
     icon: <Companies />,
     eventKey: '7',
   },
   {
+    title: 'Vendors',
     link: 'Vendors',
     icon: <Vendors />,
     eventKey: '8',
   },
   {
+    title: 'Departments',
     link: 'Departments',
     icon: <Departments />,
     eventKey: '9',
   },
   {
+    title: 'Locations',
     link: 'Locations',
     icon: <Locations />,
     eventKey: '10',
   },
   {
+    title: 'Others',
     link: 'Others',
     icon: <Other />,
     eventKey: '11',
     children: [
-      { link: 'Others/CostCenters' },
-      { link: 'Other Child 2' },
-      { link: 'Other Child 3' },
+      { link: 'Others/CostCenters', title: 'Cost Centers' },
+      { link: 'Others/Checkouts', title: 'Checkouts' },
+      { link: 'Others/ExitTypes', title: 'Exit Types' },
     ],
   },
   {
+    title: 'Contracts',
     link: 'Contracts',
     icon: <Contracts />,
     eventKey: '12',
@@ -99,47 +112,52 @@ const linkListProperties: LinkListType[] = [
 ];
 const linkListUser: LinkListType[] = [
   {
+    title: 'Users',
     link: 'Users',
     icon: <Dashboard />,
     eventKey: '13',
   },
   {
+    title: 'Roles',
     link: 'Roles',
     icon: <Dashboard />,
     eventKey: '14',
   },
   {
-    link: 'Role Authorization',
+    title: 'Role Autorization',
+    link: 'RoleAuthorization',
     icon: <Dashboard />,
     eventKey: '15',
   },
 ];
 console.log(linkListDashboard, linkListManagement, linkListProperties, linkListUser);
 
-// const panelStyles = {
-//   padding: '15px 10px',
-//   color: '#aaa',
-// };
-
 const mappedLinks = (linkList: LinkListType[]) => {
   return linkList.map((list) => {
     if (list.children && list.children.length) {
       return (
-        <Dropdown eventKey={list.eventKey} title={list.link} icon={list.icon}>
+        <Dropdown key={list.link} eventKey={list.eventKey} title={list.title} icon={list.icon}>
           {list.children.map((listChildren, index) => (
             <Dropdown.Item
+              key={listChildren.link}
               to={listChildren.link}
               as={NavLink}
               eventKey={`${list.eventKey}-${index + 1}`}
             >
-              {listChildren.link}
+              {listChildren.title}
             </Dropdown.Item>
           ))}
         </Dropdown>
       );
     } else {
       return (
-        <Nav.Item as={NavLink} to={list.link} eventKey={list.eventKey} icon={list.icon}>
+        <Nav.Item
+          key={list.link}
+          as={NavLink}
+          to={list.link}
+          eventKey={list.eventKey}
+          icon={list.icon}
+        >
           {list.link}
         </Nav.Item>
       );
